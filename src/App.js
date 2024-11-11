81d70cae564caadbbcbc6d60f73842f35cccdb21
Aaron Alexander <a.alexander72003@gmail.com>
2024-11-11 16:32:22 -0600

    feat: add landing page frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import SignUpPage from './components/SignUpPage';
+import LandingPage from './components/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div>
-            <SignUpPage />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPage />}/>
+                    <Route path="/signup" element={<SignUpPage />}/>
+                </Routes>
+            </Router>
         </div>
     );
 }
